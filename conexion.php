e6025cde18a1f24a5509b38a202498e57c79459b
Ana María <moraviedaanamaria@gmail.com>
2024-09-25 16:39:44 -0500

    trabajo en la conexion

--- conexion.php
+++ conexion.php
@@ -7,12 +7,12 @@ class Conexion {
         $host = "localhost";  
         $dbname = "reto."; 
         $username = "postgres";
-        $password = "luisa11";
+        $password = "ana1203";
 
         try {
             $this->con = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $username, $password);
             $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Se conectó correctamente a la base de datos";
+            echo "";
         } catch (PDOException $exp) {
             echo "No se puede conectar a la base de datos: " . $exp->getMessage();
         }
